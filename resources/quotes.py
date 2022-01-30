afd6c0f76d2385d529e6aa469251f9bf40b5b926
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-01-30 19:59:50 +0530

    Create quotes.py

---
+++ resources/quotes.py
@@ -0,0 +1,7 @@
+#playlist for now playing
+
+MUSIC_LIST = [
+    "Now Playing : you're mine - Kina | t.me/my_thingsuwu",
+    "Now Playing : Good 4 you - Olivia Rodriguez | t.me/my_thingsuwu"]
+
+ENGLISH_QUOTES = [ "ara ara" ]
