994056aa89710c611bd7f8c44099e9320bdef77f
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-02-21 22:42:31 +0530

    Update wally.py

--- plugins/wally.py
+++ plugins/wally.py
@@ -31,7 +31,7 @@ async def wall_(msg: Message):
         qu = msg.filtered_input_str
         await msg.edit(f"`Seraching Wallpapers for {qu}`")
         results = requests.get(
-            "https://api.unsplash.com/search/"
+            "https://wallhaven.cc/api/v1/search"
             f"photos?client_id=HWlOs9dNZIbYEkjp87fiEzC9rmE6rKM64tBqXBOLzu8&query={qu}"
         )
 
